44efebd28609611c392fde2fb4e83c9f63c71b51
Alfredo Inostroza <jadenguy@gmail.com>
2019-10-07 16:00:38 -0400

    better

--- sketch.js
+++ sketch.js
@@ -1,32 +1,36 @@
-let samplesPerCycle = 35;
+let samplesPerCycle = 100;
 let wave;
 let period;
 let count;
-let fft = [];
-let place = .1;
-let zoom = 300;
+let freqGranularity = .1;
+let fft = new Array(samplesPerCycle * 2);
 function setup() {
   createCanvas(windowWidth, windowHeight - 4);
   background(220);
   stroke(0);
   fill(255);
-  zoom = width / 7;
+  fft.fill(0, 0, fft.length);
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight - 4);
-  zoom = width / 7;
 }
 function draw() {
   background(220);
   period = TWO_PI / samplesPerCycle;
-  count = samplesPerCycle * 10;
-  wave = generateSamples(0, period, count, Math.sin);
-  place = TWO_PI * (frameCount / 120);
-  const rotated = drawCircle(wave, place);
-  fft.push(rotated.mag() / zoom);
+  count = samplesPerCycle;
+  let func = x => Math.sin(x)
+  wave = generateSamples(0, period, count, func);
+  const freq = (frameCount * freqGranularity);
+  const avg = drawCircle(wave, freq);
+  fft[frameCount - 1] = avg.mag();
+  if (avg.mag() > .2) {
+    print(freq/TWO_PI);
+  }
   drawWave(wave);
   drawWave(fft, 2);
-  // if (frameCount % 1000 == 0) { noLoop(); }
+  if (freq >= samplesPerCycle * 2 / freqGranularity) {
+    noLoop();
+  }
 }
 function clamp(num, min, max) {
   return num <= min ? min : num >= max ? max : num;
@@ -43,6 +47,7 @@ function drawCircle(wave, rate) {
   translate(width / 2, height / 2);
   let x = [];
   let y = [];
+  let zoom = width / 7;
   for (let index = 1; index < wave.length; index++) {
     const start = createVector(wave[index - 1] * zoom);
     start.rotate(rate * (index - 1));
@@ -52,12 +57,11 @@ function drawCircle(wave, rate) {
     }
     const end = createVector(wave[index] * zoom);
     end.rotate(rate * index);
+    line(start.x, start.y, end.x, end.y);
     x.push(end.x);
     y.push(end.y);
-    line(start.x, start.y, end.x, end.y);
+
   }
-
-
   ellipseMode(RADIUS);
   strokeWeight(2);
   fill(255);
@@ -65,12 +69,18 @@ function drawCircle(wave, rate) {
   const yAvg = average(y)
   circle(xAvg, yAvg, 10);
   pop();
-  return createVector(xAvg, yAvg);
+  return createVector(xAvg / zoom, yAvg / zoom);
 }
 function drawWave(wave, third = 0) {
   push();
-  translate(width / 3 * third, height / 2);
-  newWidth = width / wave.length / 3;
+  const min = Math.min(...wave);
+  const max = Math.max(...wave);
+  const zoom = .9 * height / (max - min);
+  const xStart = width / 3 * third;
+  const yStart = height * .95 + min * zoom;
+
+  translate(xStart, yStart);
+  const newWidth = width / wave.length / 3;
   for (let index = 1; index < wave.length; index++) {
     const start = createVector(newWidth * (index - 1), wave[index - 1] * -zoom);
     const end = createVector(newWidth * index, wave[index] * -zoom);
@@ -78,4 +88,7 @@ function drawWave(wave, third = 0) {
   }
   pop();
 }
-function average(arr) { return arr.reduce((a, b) => a + b, 0) / arr.length }
+function average(arr) {
+  return arr.reduce((a, b) => a + b, 0) / arr.length
+}
+
