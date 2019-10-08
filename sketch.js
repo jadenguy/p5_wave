be920940d06f8bdd7cf96c6accb2e31cbb26d64c
Alfredo Inostroza <jadenguy@gmail.com>
2019-10-07 22:34:04 -0400

    still trying to figure out the ideal step

--- sketch.js
+++ sketch.js
@@ -1,9 +1,7 @@
-let samplesPerCycle = 100;
-let cycleInTwoPi = 10;
 let wave;
-let period;
-let count;
-let freqGranularity = .01;
+let samplesPerCycle = 360;
+let cycleInTwoPi = 2;
+let freqGranularity = .00005;
 let fft = new Array(samplesPerCycle * 2);
 
 
@@ -15,45 +13,55 @@ function setup() {
   fft.fill(0, 0, fft.length);
   period = TWO_PI / samplesPerCycle;
   count = samplesPerCycle * cycleInTwoPi;
-  let func = x => Math.sin(x / 10) + 7
+  // let func = x => Math.sin(x * 3) + Math.sin(x * 7)
+  let func = x => Math.sin(x)
   wave = generateSamples(0, period, count, func);
   wave = normalizeWave(wave);
-
-
-}
-function normalizeWave(wave) {
-  const avg = average(wave);
-  for (let index = 0; index < wave.length; index++) {
-    wave[index] -= avg;
-  }
-  return wave;
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight - 4);
 }
 function draw() {
   background(220);
-  const freq = (frameCount * freqGranularity);
+  const freq = frameCount * freqGranularity;
   const avg = drawCircle(wave, freq);
-  fft[frameCount - 1] = avg.mag();
-  if (avg.mag() > .2) {
-    print(Math.round(freq / TWO_PI, freqGranularity));
+  const v = Math.abs(avg.mag());
+  fft[frameCount - 1] = v;
+  if (v > .2) {
+    print(frameCount, freq, v);
   }
-  drawWave(wave);
+  drawWave(wave, 0);
   drawWave(fft, 2);
   if (freq >= samplesPerCycle * 2 / freqGranularity) {
     noLoop();
+    print(frameCount, freq, v);
   }
 }
 function clamp(num, min, max) {
   return num <= min ? min : num >= max ? max : num;
 }
+function average(arr) {
+  return arr.reduce((a, b) => a + b, 0) / arr.length
+}
 function generateSamples(start, period, count, func) {
   var ret = [];
-  for (let x = start; x < count * period; x += period) {
+  for (let x = start; x <= count * period; x += period) {
     ret.push(func(x));
   }
   return ret;
+}
+function normalizeWave(wave) {
+  const avg = average(wave);
+  for (let index = 0; index < wave.length; index++) {
+    wave[index] -= avg;
+  }
+  const neg = -Math.min(...wave);
+  const pos = Math.max(...wave);
+  const largest = Math.max(pos, neg);
+  for (let index = 0; index < wave.length; index++) {
+    wave[index] /= largest;
+  }
+  return wave;
 }
 function drawCircle(wave, rate) {
   push();
@@ -61,19 +69,16 @@ function drawCircle(wave, rate) {
   let x = [];
   let y = [];
   let zoom = Math.min(width / 7, height / 2.2);
-  const min = Math.min(...wave);
-  const max = Math.max(...wave);
-  // zoom /= (max - min);
-  for (let index = 1; index < wave.length; index++) {
-    const start = createVector(wave[index - 1] * zoom);
-    start.rotate(rate * (index - 1));
-    if (x.length == 0) {
-      x.push(start.x);
-      y.push(start.y);
-    }
+  for (let index = 0; index < wave.length; index++) {
+    // const start = createVector(wave[index - 1] * zoom);
+    // start.rotate(rate * (index - 1));
+    // if (x.length == 0) {
+    // x.push(start.x);
+    // y.push(start.y);
+    // }
     const end = createVector(wave[index] * zoom);
     end.rotate(rate * index);
-    line(start.x, start.y, end.x, end.y);
+    point(end.x, end.y);
     x.push(end.x);
     y.push(end.y);
 
@@ -104,7 +109,3 @@ function drawWave(wave, third = 0) {
   }
   pop();
 }
-function average(arr) {
-  return arr.reduce((a, b) => a + b, 0) / arr.length
-}
-
